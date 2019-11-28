4826b0baffbb969bc78806123f8e0aec0116c173
oliver wilkes <oliverwilkes22@gmail.com>
2019-11-28 21:05:07 +0000

    added new passwords to users.

--- SOFT252CW/src/soft252cw/Hospital.java
+++ SOFT252CW/src/soft252cw/Hospital.java
@@ -23,12 +23,12 @@ public ArrayList<HospitalPerson> people;
          * Patient(String name, String surname, String address, String id, int age, String gender,String password)
          */
         Administrator admin = new Administrator("grace","porter","79 Salisbury Rd, Plymouth PL4 8TB","A2345","password");
-        Secretary secretary = new Secretary("oscar","reid","16 Lorrimore Ave, Plymouth PL2 1HZ","S4992","password"); 
-        Doctor doctor1 = new Doctor("hilda","owen","19 Baring St, Plymouth PL4 8NF","D0063","password");
-        Doctor doctor2 = new Doctor("benjamin","farrell","9 Congreve Gardens, Plymouth PL5 3HG","D5453","password");
-        Doctor doctor3 = new Doctor("david","standley","35 Dale Gardens, Plymouth PL4 6PY","D9974","password");
+        Secretary secretary = new Secretary("oscar","reid","16 Lorrimore Ave, Plymouth PL2 1HZ","S4992","123456"); 
+        Doctor doctor1 = new Doctor("hilda","owen","19 Baring St, Plymouth PL4 8NF","D0063","abc123");
+        Doctor doctor2 = new Doctor("benjamin","farrell","9 Congreve Gardens, Plymouth PL5 3HG","D5453","111111");
+        Doctor doctor3 = new Doctor("david","standley","35 Dale Gardens, Plymouth PL4 6PY","D9974","123123");
         Patient patient1 = new Patient("ian","smith","19 Effingham Cres, Plymouth PL3 5TN","P5433" ,"qwerty",25,"male");
-        Patient patient2 = new Patient("stacy","johnson","3 Baytree Gardens, Plymouth PL2 2QQ","P8253" ,"password",62,"female");
+        Patient patient2 = new Patient("stacy","johnson","3 Baytree Gardens, Plymouth PL2 2QQ","P8253" ,"iloveyou",62,"female");
         Patient patient3 = new Patient("charlotte","wood","1 Swale Cl, Plymouth PL3 6NY","P0824","logmein",33,"female");          
         
         people.add(admin);
@@ -40,6 +40,4 @@ public ArrayList<HospitalPerson> people;
         people.add(patient2);
         people.add(patient3);
     }
-
-
 }
